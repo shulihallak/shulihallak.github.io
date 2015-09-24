5fc04b1c315a27a6fbe7fab0e00f49aa6cc68be8
shulihallak <shuli@invisiblenetworks.co>
2015-09-24 13:35:58 -0400

    added click function for 'X'

--- Project1/tictactoe.js
+++ Project1/tictactoe.js
@@ -1,34 +1,79 @@
-var board = [0,0,0,0,0,0,0,0,0];
+var board = $('.container');
 
-var labels = 'abcdefghi';
+var boxes = $('.col-md-4');	
 
-// var wins = [
-// 	[a,b,c]
-// 	[d,e,f]
-// 	[h,i,j]
-// 	[a,d,g]
-// 	[b,e,h]
-// 	[c,f,i]
+var values = ['X', 'O'];
 
-// ];
+var box1 = $('#a'),
+ 	box2 = $('#b'),
+ 	box3 = $('#c'),
+ 	box4 = $('#d'),
+ 	box5 = $('#e'),
+ 	box6 = $('#f'),
+ 	box7 = $('#g'),
+ 	box8 = $('#h'),
+ 	box9 = $('#i');
+
 
 var Game = {
 	playerScore: 0,
 	computerScore: 0,
 
-	updateScore: function (scoreIncrement) {
-		this.score +=scoreIncrement;
+
+
+	updatePlayerScore: function () {
+		this.playerScore +=1;
+	},
+	updateComputerScore: function () {
+		this.computerScore +=1;
 	},
 	updateScoreView: function() {
 		$('.badge').text("Current Player Score: " + this.playerScore + "\n" + "Current Computer Score" + this.computerScore );
-	}
+	},
+
+	setListeners: function () {
+		var theGame = this;
+
+		$('.container').on('click', function (e) {
+			console.log(e.target.id);
+			var boxID = e.target.id;
+			})
+	},
 	
+	setX: function () {
+		$(box1).on('click', function (){
+			$(box1).text('X');
+		})
+		$(box2).on('click', function (){
+			$(box2).text('X');
+		})
+		$(box3).on('click', function (){
+			$(box3).text('X');
+		})
+		$(box4).on('click', function (){
+			$(box4).text('X');
+		})
+		$(box5).on('click', function (){
+			$(box5).text('X');
+		})
+		$(box6).on('click', function (){
+			$(box6).text('X');
+		})
+		$(box7).on('click', function (){
+			$(box7).text('X');
+		})
+		$(box8).on('click', function (){
+			$(box8).text('X');
+		})
+		$(box9).on('click', function (){
+			$(box9).text('X');
+		})
+	}	
+		
+
 }
+	
 
-var player = {
-
-}
-
-var autoplayer = {
-
+function determineWinner () {
+	
 }
